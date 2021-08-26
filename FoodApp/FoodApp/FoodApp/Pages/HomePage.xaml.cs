9262f34723dcaf8e4d3ed759633f59f325f68eb4
othmane-qr <othmane.qorqach@gmail.com>
2021-08-26 15:58:26 +0100

    Update Popular Products

--- FoodApp/FoodApp/FoodApp/Pages/HomePage.xaml.cs
+++ FoodApp/FoodApp/FoodApp/Pages/HomePage.xaml.cs
@@ -83,5 +83,13 @@ namespace FoodApp.Pages
             ((CollectionView)sender).SelectedItem = null;
 
         }
+
+        private void CvProducts_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var currentSelection = e.CurrentSelection.FirstOrDefault() as PopularProduct;
+            if (currentSelection == null) return;
+            Navigation.PushModalAsync(new ProductDetailPage(currentSelection.id));
+            ((CollectionView)sender).SelectedItem = null;
+        }
     }
 }
