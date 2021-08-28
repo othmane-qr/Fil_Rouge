eef3de0d4fa2f5e95613b8babef056d7abd988b7
othmane-qr <othmane.qorqach@gmail.com>
2021-08-28 12:22:33 +0100

    Update Data binding add to cart

--- FoodApp/FoodApp/FoodApp/Pages/CartPage.xaml.cs
+++ FoodApp/FoodApp/FoodApp/Pages/CartPage.xaml.cs
@@ -21,20 +21,32 @@ namespace FoodApp.Pages
             InitializeComponent();
             CartItems = new ObservableCollection<ShoppingCartItem>();
             GetShoppingCartItems();
+          
         }
+
+       /* private async void GetTotalPrice()
+        {
+            var apiService = new ApiService();
+            var totalPrice = await apiService.GetCartSubTotal(Preferences.Get("userID", 0));
+            LblTotalPrice.Text = totalPrice.subTotal.ToString();
+        }*/
 
         private async void GetShoppingCartItems()
         {
 
             var apiService = new ApiService();
             var shoppingCartItems = await apiService.GetShoppingCartItems(Preferences.Get("userID", 0));
+            double total = new double();
+            /*Product product = null;*/
             foreach (var cartItem in shoppingCartItems)
             {
                 CartItems.Add(cartItem);
-                
+                total += cartItem.TotalAmount;
                 /*product = await apiService.GetProductByID(cartItem.ProductId);*/
+
             }
             LvShoppingCart.ItemsSource = CartItems;
+            LblTotalPrice.Text = total.ToString();
         }
 
         private void TapBack_Tapped(object sender, EventArgs e)
